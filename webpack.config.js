fd98b8c63f30ca51bfea12e2568463c47a6f651d
Nathan Witt <wittnl@gmail.com>
2017-09-21 23:51:18 -0500

    Switches entry point to webpack output lib/treedux.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './index.js',
+  output: {
+    filename: 'treedux.js',
+    library: 'treedux',
+    libraryTarget: 'umd',
+    path: __dirname + '/lib'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'babel-loader' }
+      }
+    ]
+  },
+  plugins: [
+    // new UglifyJSPlugin({sourceMap: true})
+  ]
+};
